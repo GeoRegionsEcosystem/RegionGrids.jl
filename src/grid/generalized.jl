d25e78524c2b39df3a7df4ce6f1a07abcdbbe0bf
Nathanael Wong <natgeo.wong@outlook.com>
2025-02-12 04:46:51 -0500

    No more differentiation between RectRegion, PolyRegion and TiltRegion, consistent with updated GeoRegions.jl struct types

--- src/grid/generalized.jl
+++ src/grid/generalized.jl
@@ -1,77 +1,8 @@
 """
     RegionGrid(
-        geo  :: Union{RectRegion,PolyRegion},
+        geo  :: GeoRegion,
         pnts :: Array{Point2{FT}}
-    ) where FT <: Real -> ggrd :: GeneralMask
-
-Creates a `GeneralMask` type based on the following arguments. This method is more suitable for structured non-rectilinear (e.g., curvilinear) grids of longitude and latitude points, such as in the output of WRF.
-
-Arguments
-=========
-- `geo` : A GeoRegion of interest
-- `pnts` : An array of `Point2` types containing the longitude/latitude points
-
-Returns
-=======
-- `ggrd` : A `GeneralMask`
-"""
-function RegionGrid(
-    geo  :: Union{RectRegion,PolyRegion},
-    pnts :: Array{Point2{FT}}
-) where FT <: Real
-
-    @info "$(modulelog()) - Creating a GeneralizedGrid for the $(geo.name) GeoRegion"
-
-    _,_,E,W = geo.bound
-
-    @debug "$(modulelog()) - Determining indices of longitude and latitude boundaries in the given dataset ..."
-
-    nlon,nlat = size(pnts)
-    iW = nlon; iE = 1
-    iS = nlat; iN = 1
-
-    for iilat = 1 : nlat, iilon = 1 : nlon
-        if in(pnts[iilon,iilat],geo)
-            iS = min(iS,iilat); iN = max(iN,iilat)
-            iW = min(iW,iilon); iE = max(iE,iilon)
-        end
-    end
-
-    iWE = iW:iE; nlon = length(iWE)
-    iSN = iS:iN; nlat = length(iSN)
-
-    lon  = zeros(nlon,nlat)
-    lat  = zeros(nlon,nlat)
-    ilon = zeros(nlon,nlat)
-    ilat = zeros(nlon,nlat)
-    mask = zeros(nlon,nlat)
-    wgts = zeros(nlon,nlat)
-
-    for iilat = 1 : nlat, iilon = 1 : nlon
-        iiWE = iWE[iilon]; ilon[iilon,iilat] = iiWE
-        iiSN = iSN[iilat]; ilat[iilon,iilat] = iiSN
-        lon[iilon,iilat] = pnts[iiWE,iiSN][1]
-        if lon[iilon,iilat] > E; lon[iilon,iilat] -= 360 end
-        if lon[iilon,iilat] < W; lon[iilon,iilat] += 360 end
-        lat[iilon,iilat] = pnts[iiWE,iiSN][2]
-        if in(pnts[iiWE,iiSN],geo)
-            mask[iilon,iilat] = 1
-            wgts[iilon,iilat] = cosd.(lat[iilon,iilat])
-        else
-            mask[iilon,iilat] = NaN
-            wgts[iilon,iilat] = 0
-        end
-    end
-
-    return GeneralMask{FT}(lon,lat,ilon,ilat,mask,wgts)
-
-end
-
-"""
-    RegionGrid(
-        geo  :: TiltRegion,
-        pnts :: Array{Point2{FT}}
-    ) where FT <: Real -> ggrd :: GeneralTilt
+    ) where FT <: Real -> ggrd :: GeneralizedGrid
 
 Creates a `GeneralTilt` type based on the following arguments. This method is more suitable for structured non-rectilinear (e.g., curvilinear) grids of longitude and latitude points, such as in the output of WRF.
 
@@ -85,7 +16,7 @@ Returns
 - `ggrd` : A `GeneralTilt`
 """
 function RegionGrid(
-    geo  :: TiltRegion,
+    geo  :: GeoRegion,
     pnts :: Array{Point2{FT}}
 ) where FT <: Real
 
@@ -149,6 +80,6 @@ function RegionGrid(
         end
     end
 
-    return GeneralTilt{FT}(lon,lat,ilon,ilat,mask,wgts,rotX,rotY)
+    return GeneralizedGrid{FT}(lon,lat,ilon,ilat,mask,wgts,rotX,rotY)
 
 end
